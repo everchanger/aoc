076be3dd5850cb19b8392396191aa31afd0229ee
Joakim Rosenstam <joakim.rosenstam@gmail.com>
2022-11-30 08:22:16 +0100

    remove auto submit

--- src/aoc.js
+++ src/aoc.js
@@ -8,19 +8,4 @@ const fetchInput = async (year, day, token) => {
   return response.text()
 }
 
-const submitAnswer = async (year, day, token, level, answer) => {
-  console.log('submitting', year, day, level, answer)
-  const response = await fetch(`https://adventofcode.com/${year}/day/${day}/answer`, {
-    method: 'POST',
-    headers: {
-      Cookie: `session=${token}`
-    },
-    body: JSON.stringify({
-      level,
-      answer
-    })
-  })
-  return (await response.text()).includes('That\'s the right answer!')
-}
-
-export { fetchInput, submitAnswer }
+export { fetchInput }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import fse from 'fs-extra'
 import path from 'path'
 import kleur from 'kleur'
 import dotenv from 'dotenv'
-import { fetchInput, submitAnswer } from './aoc.js'
+import { fetchInput } from './aoc.js'
 import { startTimer, stopTimer } from './utils.js'
 dotenv.config()
 
@@ -51,7 +51,6 @@ const prepareDay = async (dayPath, day, year) => {
 const run = async (dayPath) => {
   const { taskA, taskB, test } = await import(`${dayPath}/index.js`)
   const testInput = JSON.parse(fs.readFileSync(`${dayPath}/test.json`, 'utf8'))
-  const done = JSON.parse(fs.readFileSync(`${dayPath}/done.json`, 'utf8'))
 
   // Run tests before executing the tasks
   console.log(kleur.cyan('\nRunning tests for A...'))
@@ -72,18 +71,6 @@ const run = async (dayPath) => {
   console.log(`  Time: ${stopTimer('Timer A')}ms`)
   console.log(kleur.green('  Result:'), resultA)
 
-  // submit answer if we have not already submitted
-  if (!done.A && process.env.AOC_TOKEN && process.env.AUTO_SUBMIT === 'true') {
-    const correct = submitAnswer(year, day, process.env.AOC_TOKEN, 1, resultA)
-    if (correct) {
-      console.log(kleur.green('Solution for A accepted!'))
-      done.A = true;
-      fs.writeFileSync(`${dayPath}/done.json`, JSON.stringify(done))
-    } else {
-      console.log(kleur.red('Incorrect solution for A...'))
-    }
-  }
-
   console.log(kleur.cyan('\nRunning tests for B...'))
 
   pass = await test(testInput, 'taskB')
@@ -100,17 +87,6 @@ const run = async (dayPath) => {
   const resultB = await taskB(input)
   console.log(`  Time: ${stopTimer('Timer B')}ms`)
   console.log(kleur.green('  Result:'), resultB)
-
-  if (!done.B && process.env.AOC_TOKEN && process.env.AUTO_SUBMIT === 'true') {
-    const correct = submitAnswer(year, day, process.env.AOC_TOKEN, 2, resultB)
-    if (correct) {
-      console.log(kleur.green('Solution for B accepted!'))
-      done.B = true;
-      fs.writeFileSync(`${dayPath}/done.json`, JSON.stringify(done))
-    } else {
-      console.log(kleur.red('Incorrect solution for B...'))
-    }
-  }
 }
 
 const { day, year } = await getDayAndYear()
